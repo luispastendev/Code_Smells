ea790cabbcc5019d3800d18a0699afc51a599ae9
Luis Pastén <luispasten.dev@gmail.com>
2021-05-15 21:40:11 -0500

    Code Smell: Null Object agregado

--- Null Object/index.php
+++ Null Object/index.php
@@ -1,55 +1,80 @@
 <?php
-
 interface Plan{
+
     function getName() : string;
 }
 
-interface ActiveCustomer{
+interface Customer{
+
     public function getCurrentPlan() : string;
 }
 
 class Free implements Plan{
+
     public function getName() : string{
         return "Plan Gratuito";
     }
 }
 
 class Premium implements Plan{
+
     public function getName() : string{
         return "Plan Premium";
     }
 }
 
-class Customer implements ActiveCustomer{
+class ActiveCustomer implements Customer{
 
     protected $plan; 
 
-    public function __construct($plan){
+    public function __construct($plan = null){
 
         $this->plan = $plan; 
     }
 
     public function getCurrentPlan() : string{
+
         return $this->plan->getName();
     }
 }
 
+/**
+ * Clase generica que sobrescribe el metodo getCurrentPlan
+ * para devolver una clase por default "Free" y a su ves 
+ * extiende los metodos ActiveCustomer para utilizarlos
+ */
+class GenericCustomer extends ActiveCustomer{
+
+    public function getCurrentPlan() : string {
+
+        return (new Free())->getName();
+    }
+
+    // ... mas metodos nulos que necesites sobrescribir
+}
+
 class Order{
     public $customer;
-
-    public function __invoke($customer = null){
+    /** 
+     * el magic method invoke permite llamar a una clase como funcion 
+     * y en este caso simplemente guarda una instancia de las clases 
+     * de tipo Customer (ActiveCustomer / GenericCustomer)
+     */  
+    public function __invoke(Customer $customer = null){
         $this->customer = $customer;
     }
 }
 
 function getPlan(Order $order) : string{
-    // SET PLAN
-    if($order->customer === null){
-        $plan = (new Free())->getName();
-    }else{
-        $plan = $order->customer->getCurrentPlan();
-    }
-    return $plan;
+    /**
+     * Si customer regresa null instanciamos un objeto generico que tiene
+     * las mismas capacidades de un objeto normal (ActiveCostumer) pero
+     * con algunso métodos sobrescritos para devolver valores por default
+     * 
+     * En este caso devuelve un Plan Free si no es ningun tipo de cliente
+     */
+    $customer = !$order->customer ? new GenericCustomer() : $order->customer; 
+    return $customer->getCurrentPlan();
 }
 
 
@@ -61,10 +86,20 @@ $orders = [
 
 
 foreach($orders as $k => $order){
-    $k === 'ord_1' || $k === 'ord_2' ? $order(new Customer(new Premium)) : $order();
+    /**
+     * Para efectos del ejemplo incializamos los objetos ord_1 y ord_2 con una isntancia de 
+     * active customer y ord_3 sin nigun consumidor asignado
+     */
+    $k === 'ord_1' || $k === 'ord_2' ? $order(new ActiveCustomer(new Premium)) : $order();
 }
-// var_dump($orders);
 
 foreach($orders as $order){
     echo getPlan($order) . "\n";
+    /**
+     * Resultado esperado:
+     * 
+     * Plan Premium
+     * Plan Premium
+     * Plan Gratuito
+     */
 }
